9c3d39eef30ea67ff28d8eacf88f95aa47f1b162
rrobert45 <rrobert45@gmail.com>
2023-03-03 10:41:54 -0500

    updates

--- webapp.py
+++ webapp.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from statistics import mean, pstdev
 import numpy as np
 from datetime import datetime, timedelta
+from math import floor
 
 
 with open('config.json') as config_file:
@@ -29,11 +30,11 @@ def lock_down_and_hatch(start_date):
 @app.route('/')
 def index():
     lock_down_date, hatch_date = lock_down_and_hatch(start_date)
-    cursor = incubator.find().sort("Time", -1)
+    cursor = incubator.find().sort("Timestamp", -1)
     historical_data = []
     for data in cursor:
         historical_data.append({
-            'Time': data['Time'],
+            'Time': data['Timestamp'],
             'Temperature(F)': data['Temperature(F)'],
             'Temperature Relay Status': data['Temperature Relay Status'],
             'Humidity(%)': data['Humidity(%)'],
@@ -65,6 +66,26 @@ def index():
         'current_data': current_data,  # Add current data to the dictionary
     }
     return render_template('index.html', data=data)
+
+@app.route('/historical_data')
+def historical_data():
+    cursor = incubator.find().sort("Timestamp", -1)
+    historical_data = []
+    for data in cursor:
+        historical_data.append({
+            'Time': data['Timestamp'],
+            'Temperature(F)': data['Temperature(F)'],
+            'Temperature Relay Status': data['Temperature Relay Status'],
+            'Humidity(%)': data['Humidity(%)'],
+            'Humidity Relay Status': data['Humidity Relay Status'],
+            'Last Egg Turn': data['Last Egg Turn'],
+            'Day in Egg Cycle': data['Day in Egg Cycle']
+        })
+    return render_template('historical_data.html', historical_data=historical_data)
+
+@app.context_processor
+def inject_nav_links():
+    return {'nav_links': [{'url': '/', 'label': 'Home'}, {'url': '/historical_data', 'label': 'Historical Data'}]}
 
 
 def get_egg_cycle_statistics(historical_data):
@@ -100,14 +121,15 @@ def get_egg_cycle_statistics(historical_data):
     return egg_cycle_statistics
 
 
+
 def get_egg_cycle_statistics_by_hour(historical_data):
     egg_cycle_dict = {}
     for data in historical_data:
         day = data['Day in Egg Cycle']
         temperature = data['Temperature(F)']
         humidity = data['Humidity(%)']
-        timestamp = data['Time'].strftime('%Y-%m-%d %H:%M:%S')
-        hour = timestamp.hour
+        timestamp = data['Time']
+        hour = floor(timestamp.timestamp() / 3600)
         if day in egg_cycle_dict:
             if hour in egg_cycle_dict[day]:
                 egg_cycle_dict[day][hour]['temperature'].append(temperature)
@@ -132,16 +154,16 @@ def get_egg_cycle_statistics_by_hour(historical_data):
             std_temp = np.std(values['temperature'])
             avg_hum = np.mean(values['humidity'])
             std_hum = np.std(values['humidity'])
+            timestamp = datetime.fromtimestamp(hour * 3600)
             egg_cycle_statistics.append({
-                'Day in Egg Cycle': day,
-                'Hour': hour,
+                'Time': timestamp.strftime('%m-%d-%Y %H:%M'),
                 'Average Temperature (F)': round(avg_temp, 2),
                 'Temperature Standard Deviation': round(std_temp, 2),
                 'Average Humidity (%)': round(avg_hum, 2),
                 'Humidity Standard Deviation': round(std_hum, 2),
             })
         
-    egg_cycle_statistics.sort(key=lambda x: (x['Day in Egg Cycle'], x['Hour']), reverse=True)
+    egg_cycle_statistics.sort(key=lambda x: x['Time'], reverse=True)
     return egg_cycle_statistics
 
 
